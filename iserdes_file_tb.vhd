d90444596eb956922d868529fc7b0f525e0febbe
DESKTOP-R4L1JTG\jae <jaehyek@gmail.com>
2020-05-11 03:56:26 +0900

    save

--- iserdes_file_tb.vhd
+++ iserdes_file_tb.vhd
@@ -83,7 +83,7 @@ ARCHITECTURE behavior OF tb_file IS
 	signal inclk_p : std_logic := '1' ;
 
 	signal serdes_reset   : std_logic;
-	signal deser_data     : std_logic_vector(49 downto 0);
+	signal deser_in     : std_logic_vector(49 downto 0);
 	signal byte_clock_int : std_logic;
 
 
@@ -102,6 +102,19 @@ ARCHITECTURE behavior OF tb_file IS
 	signal csi_in_line          : std_logic;
 
 	signal word_valid : std_logic;
+
+
+	----------------------------------------------------------------------------
+	-- csi_rx_byte_align
+	signal curr_byte    : std_logic_vector(9 downto 0);
+    signal last_byte    : std_logic_vector(9 downto 0);
+    signal shifted_byte : std_logic_vector(9 downto 0);
+
+    signal found_hdr      : std_logic;
+    signal byte_valid_int : std_logic;
+    signal hdr_offs       : unsigned(3 downto 0);
+    signal data_offs      : unsigned(3 downto 0);
+    signal data_off_lock  : std_logic;
 
 
 
@@ -123,7 +136,7 @@ BEGIN
 		variable val_comma                 : character; -- for commas between data in file
 		variable good                      : boolean;
 
-		variable file_deser_data     : std_logic_vector ( 49 downto 0 );
+		variable file_deser_in     : std_logic_vector ( 49 downto 0 );
 		variable file_byte_clock_int : std_logic ;
 		variable file_found_hdr4     : std_logic ;
 		variable file_found_hdr2     : std_logic ;
@@ -148,8 +161,8 @@ BEGIN
 			assert good report "Text I/O read error: trigger" severity ERROR;
 
 			read(read_col_from_input_buf, val_comma);
-			hread(read_col_from_input_buf, file_deser_data, good);
-			assert good report "Text I/O read error: file_deser_data" severity ERROR;
+			hread(read_col_from_input_buf, file_deser_in, good);
+			assert good report "Text I/O read error: file_deser_in" severity ERROR;
 
 			read(read_col_from_input_buf, val_comma);
 			read(read_col_from_input_buf, file_byte_clock_int, good);
@@ -187,9 +200,13 @@ BEGIN
 			read(read_col_from_input_buf, file_packet_done, good);
 			assert good report "Text I/O read error: file_packet_done" severity ERROR;
 
-			deser_data   <= file_deser_data ;
+			deser_in   <= file_deser_in ;
 			-- serdes_reset <= file_serdes_reset ;
 			byte_clock_int <= file_byte_clock_int;
+			packet_start <= '0' ;
+			packet_end_invalid <= '0';
+			packet_end <= '0' ;
+
 
 			wait for period;
 
@@ -200,52 +217,193 @@ BEGIN
 
 	end process ;
 
+	--process(byte_clock_int)
+ --   begin
+ --       if rising_edge(byte_clock_int) then
+ --           if serdes_reset = '1' then
+ --               byte_valid_int <= '0';
+ --               data_offs      <= "0000";
+ --               data_off_lock  <= '0';
+ --           elsif enable = '1' then
+ --               last_byte       <= curr_byte;
+ --               curr_byte       <= deser_in(9 downto 0 );
+ --               byte_align_data(9 downto 0 ) <= shifted_byte;
 
-	----------------------------------------------------------------------------
-	----------------------------------------------------------------------------
-	gen_bytealign : for i in 0 to 4 generate
-		ba : csi_rx_byte_align
-			port map (
-				clock              => byte_clock_int,                           -- in 
-				reset              => serdes_reset,                             -- in 
-				enable             => enable,                                   -- in 
-				deser_in           => deser_data((10*i) + 9 downto 10 * i),     -- in 
-				packet_start       => packet_start,                             -- in 
-				packet_end_invalid => packet_end_invalid,                       -- in 
-				byte_valid         => byte_valid(i),                            -- out
-				byte_align_data    => byte_align_data((10*i) + 9 downto 10 * i) -- out
-			);
-	end generate;
+ --               --if packet_end_invalid = '1' then
+ --               --    byte_valid_int <= found_hdr;
+ --               --elsif packet_start = '1' and found_hdr = '1' and byte_valid_int = '0' then
+ --               --    byte_valid_int <= '1';
+ --               --    data_offs      <= hdr_offs;
+ --               --end if;
 
-	wordalign : csi_rx_word_align
+ --               if packet_start = '1' then 
+ --                   data_off_lock <= '1' ;
+ --               elsif packet_end_invalid = '1' then
+ --                   data_off_lock <= '0';
+ --               end if ;
+                    
+ --               if data_off_lock = '1' then 
+ --                   byte_valid_int <= '1';
+ --               else 
+ --                   if found_hdr = '1' then 
+ --                       data_offs      <= hdr_offs;
+ --                       byte_valid_int <= '1';
+ --                    else
+ --                    	byte_valid_int <= '0';
+ --                   end if ;
+ --               end if ;
+ --           end if;
+ --       end if;
+ --   end process;
+
+ --   byte_valid(0) <= byte_valid_int;
+ --   --This assumes that data is arranged correctly (chronologically last bit in MSB)
+ --   --and looks for the "10111000" sync sequence
+
+
+ --   process(curr_byte, last_byte)
+ --       constant sync      : std_logic_vector(9 downto 0) := "1110100110";
+ --       variable was_found : boolean                      := false;
+ --       variable offset    : integer range 0 to 9;
+ --   begin
+ --       offset    := 0;
+ --       was_found := false;
+ --       for i in 0 to 9 loop
+ --           if (last_byte(i-1 downto 0) & curr_byte(9 downto i ) = sync) then --  just for mipi  :  and (unsigned(last_byte(9 downto i)) = 0) then
+ --               was_found := true;
+ --               offset    := i;
+ --           end if;
+ --       end loop;
+ --       if was_found then
+ --           found_hdr <= '1';
+ --           hdr_offs  <= to_unsigned(offset, 4);
+ --       else
+ --           found_hdr <= '0';
+ --           hdr_offs  <= "0000";
+ --       end if;
+ --   end process;
+
+ --   --This aligns the data correctly
+
+ --   shifted_byte <= curr_byte(9 downto 0 )             when data_offs = 0 else
+ --       last_byte(0 downto 0) & curr_byte(9 downto 1 ) when data_offs = 1 else
+ --       last_byte(1 downto 0) & curr_byte(9 downto 2 ) when data_offs = 2 else
+ --       last_byte(2 downto 0) & curr_byte(9 downto 3 ) when data_offs = 3 else
+ --       last_byte(3 downto 0) & curr_byte(9 downto 4 ) when data_offs = 4 else
+ --       last_byte(4 downto 0) & curr_byte(9 downto 5 ) when data_offs = 5 else
+ --       last_byte(5 downto 0) & curr_byte(9 downto 6 ) when data_offs = 6 else
+ --       last_byte(6 downto 0) & curr_byte(9 downto 7 ) when data_offs = 7 else
+ --       last_byte(7 downto 0) & curr_byte(9 downto 8 ) when data_offs = 8 else
+ --       last_byte(8 downto 0) & curr_byte(9 downto 9 );
+
+
+
+
+	ba0 : entity work.csi_rx_byte_align
 		port map (
-			word_clock         => byte_clock_int,     -- in 
-			reset              => serdes_reset,       -- in 
-			enable             => enable,             -- in 
-			packet_end         => packet_end,         -- in 
-			packet_start       => packet_start,       -- in 
-			packet_end_invalid => packet_end_invalid, -- out
-			byte_align_data    => byte_align_data,    -- in 
-			byte_valid         => byte_valid,         -- in 
-			word_align_data    => word_align_data,    -- out
-			word_valid         => word_valid);        -- out
+			clock              => byte_clock_int,                           -- in 
+			reset              => serdes_reset,                             -- in 
+			enable             => enable,                                   -- in 
+			deser_in           => deser_in((10*0) + 9 downto 10 * 0),     -- in 
+			packet_start       => packet_start,                             -- in 
+			packet_end_invalid => packet_end_invalid,                       -- in 
+			byte_valid         => byte_valid(0),                            -- out
+			byte_align_data    => byte_align_data((10*0) + 9 downto 10 * 0) -- out
+		);
+
+	--ba1 : csi_rx_byte_align
+	--	port map (
+	--		clock              => byte_clock_int,                           -- in 
+	--		reset              => serdes_reset,                             -- in 
+	--		enable             => enable,                                   -- in 
+	--		deser_in           => deser_in((10*1) + 9 downto 10 * 1),     -- in 
+	--		packet_start       => packet_start,                             -- in 
+	--		packet_end_invalid => packet_end_invalid,                       -- in 
+	--		byte_valid         => byte_valid(1),                            -- out
+	--		byte_align_data    => byte_align_data((10*1) + 9 downto 10 * 1) -- out
+	--	);
+
+	--ba2 : csi_rx_byte_align
+	--	port map (
+	--		clock              => byte_clock_int,                           -- in 
+	--		reset              => serdes_reset,                             -- in 
+	--		enable             => enable,                                   -- in 
+	--		deser_in           => deser_in((10*2) + 9 downto 10 * 2),     -- in 
+	--		packet_start       => packet_start,                             -- in 
+	--		packet_end_invalid => packet_end_invalid,                       -- in 
+	--		byte_valid         => byte_valid(2),                            -- out
+	--		byte_align_data    => byte_align_data((10*2) + 9 downto 10 * 2) -- out
+	--	);
+
+	--ba3 : csi_rx_byte_align
+	--	port map (
+	--		clock              => byte_clock_int,                           -- in 
+	--		reset              => serdes_reset,                             -- in 
+	--		enable             => enable,                                   -- in 
+	--		deser_in           => deser_in((10*3) + 9 downto 10 * 3),     -- in 
+	--		packet_start       => packet_start,                             -- in 
+	--		packet_end_invalid => packet_end_invalid,                       -- in 
+	--		byte_valid         => byte_valid(3),                            -- out
+	--		byte_align_data    => byte_align_data((10*3) + 9 downto 10 * 3) -- out
+	--	);
+
+	--ba4 : csi_rx_byte_align
+	--	port map (
+	--		clock              => byte_clock_int,                           -- in 
+	--		reset              => serdes_reset,                             -- in 
+	--		enable             => enable,                                   -- in 
+	--		deser_in           => deser_in((10*4) + 9 downto 10 * 4),     -- in 
+	--		packet_start       => packet_start,                             -- in 
+	--		packet_end_invalid => packet_end_invalid,                       -- in 
+	--		byte_valid         => byte_valid(4),                            -- out
+	--		byte_align_data    => byte_align_data((10*4) + 9 downto 10 * 4) -- out
+	--	);
+
+	----------------------------------------------------------------------------
+	----------------------------------------------------------------------------
+	--gen_bytealign : for i in 0 to 4 generate
+	--	ba : csi_rx_byte_align
+	--		port map (
+	--			clock              => byte_clock_int,                           -- in 
+	--			reset              => serdes_reset,                             -- in 
+	--			enable             => enable,                                   -- in 
+	--			deser_in           => deser_in((10*i) + 9 downto 10 * i),     -- in 
+	--			packet_start       => packet_start,                             -- in 
+	--			packet_end_invalid => packet_end_invalid,                       -- in 
+	--			byte_valid         => byte_valid(i),                            -- out
+	--			byte_align_data    => byte_align_data((10*i) + 9 downto 10 * i) -- out
+	--		);
+	--end generate;
+
+	--wordalign : csi_rx_word_align
+	--	port map (
+	--		word_clock         => byte_clock_int,     -- in 
+	--		reset              => serdes_reset,       -- in 
+	--		enable             => enable,             -- in 
+	--		packet_end         => packet_end,         -- in 
+	--		packet_start       => packet_start,       -- in 
+	--		packet_end_invalid => packet_end_invalid, -- out
+	--		byte_align_data    => byte_align_data,    -- in 
+	--		byte_valid         => byte_valid,         -- in 
+	--		word_align_data    => word_align_data,    -- out
+	--		word_valid         => word_valid);        -- out
 
 
 	------------------------------------------------------------------------------
-	depacket : csi_rx_packet_handler
-		port map (
-			clock           => byte_clock_int,       -- in 
-			reset           => serdes_reset,         -- in 
-			enable          => enable,               -- in 
-			i_data          => word_align_data,      -- in 
-			i_data_valid    => word_valid,           -- in 
-			packet_start    => packet_start,         -- out
-			packet_end      => packet_end,           -- out
-			o_payload       => packet_payload,       -- out
-			o_payload_valid => packet_payload_valid, -- out
-			o_frame         => csi_in_frame,         -- out
-			o_line          => csi_in_line           -- out
-		);
+	--depacket : csi_rx_packet_handler
+	--	port map (
+	--		clock           => byte_clock_int,       -- in 
+	--		reset           => serdes_reset,         -- in 
+	--		enable          => enable,               -- in 
+	--		i_data          => word_align_data,      -- in 
+	--		i_data_valid    => word_valid,           -- in 
+	--		packet_start    => packet_start,         -- out
+	--		packet_end      => packet_end,           -- out
+	--		o_payload       => packet_payload,       -- out
+	--		o_payload_valid => packet_payload_valid, -- out
+	--		o_frame         => csi_in_frame,         -- out
+	--		o_line          => csi_in_line           -- out
+	--	);
 	--------------------------------------------------------------------------------
 
 
